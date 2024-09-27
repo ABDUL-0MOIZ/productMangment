94300b38cac11e3b6c6935fbd958990fcc2befb3
AbdulMoeez <moizjutt7717@gmail.com>
2024-09-27 20:01:22 +0500

    Adding file

---
+++ src/productmang/ProductMang.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package productmang;
+
+import frame.frame;
+
+/**
+ *
+ * @author ABDUL MOIZ
+ */
+public class ProductMang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        new frame().setVisible(true);
+    }
+    
+}
